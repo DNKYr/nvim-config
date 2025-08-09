96ac67e6bf43d53847aeb6bdd3e156c70faf4ac2
David Li <dnkyr2007@gmail.com>
2025-08-09 09:05:02 -0400

    resolved merge conflict

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,44 @@
+return {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- For icons
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Header
+      dashboard.section.header.val = {
+        "   Neovim from Scratch   ",
+        "   Powered by Lazy.nvim  ",
+      }
+
+      -- Buttons
+      dashboard.section.buttons.val = {
+        dashboard.button("f", "📂 Find File", ":Telescope find_files<CR>"),
+        dashboard.button("g", "🔍 Live Grep", ":Telescope live_grep<CR>"),
+        dashboard.button("gs", " Git Status", ":Git<CR>"),
+        dashboard.button("n", "📝 New File", ":ene<CR>"),
+        dashboard.button("r", "🕒 Recent Files", ":Telescope oldfiles<CR>"),
+        dashboard.button("q", "🚪 Quit", ":qa<CR>"),
+      }
+
+      -- Footer
+      dashboard.section.footer.val = "Welcome! " .. os.date("%H:%M %d-%m-%Y")
+
+      -- Apply TokyoNight highlights
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+
+      -- Setup
+      alpha.setup(dashboard.opts)
+
+      -- Disable folding
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "alpha",
+        callback = function()
+          vim.opt_local.foldenable = false
+        end,
+      })
+    end,
+  }
