14e937f8cf0ed8d5de1d5cd498086b40f12dcb0e
David Li <dnkyr2007@gmail.com>
2025-08-23 18:56:27 -0400

    Update Treesitter to contain C++

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,11 @@
 return{
-    require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+   "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "cpp", "lua", "bash", "json", "vim" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
 }
